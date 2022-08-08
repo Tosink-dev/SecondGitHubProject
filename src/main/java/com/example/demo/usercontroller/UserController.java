e4d2c91e92a21f1fa530e97b620798646d51db9e
Tosink-dev <110462935+Tosink-dev@users.noreply.github.com>
2022-08-08 11:59:15 +0100

    user home rest api created

--- src/main/java/com/example/demo/usercontroller/UserController.java
+++ src/main/java/com/example/demo/usercontroller/UserController.java
@@ -1,5 +1,18 @@
 package com.example.demo.usercontroller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+	
+	@GetMapping("/home")
+	public String home() {
+		
+		
+		return "Welcome to USER HOME PAGE";
+	}
 
 }
