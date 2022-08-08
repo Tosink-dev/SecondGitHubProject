51bd3ae9b60cc68c0ec2e0ec68d456d851c108cd
Tosink-dev <110462935+Tosink-dev@users.noreply.github.com>
2022-08-08 12:53:05 +0100

    initial feature1 commit

---
+++ src/main/java/com/example/demo/admincontroller/AminController.java
@@ -0,0 +1,18 @@
+package com.example.demo.admincontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/admin")
+public class AminController {
+
+	
+	@GetMapping("/home")
+	public String home() {
+		
+		
+		return "Welcome to ADMIN HOME PAGE";
+	}
+}
