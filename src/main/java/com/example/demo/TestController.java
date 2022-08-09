734b5ee85318129729d2a330499baa38c5f70a08
Tosink-dev <110462935+Tosink-dev@users.noreply.github.com>
2022-08-09 12:28:39 +0100

    profile page modified

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/")
 public class TestController {
 	
 	public String test () {
+		
 		return "";
+		return "Welcome to the PROFILE PAGE";
 		
 	}
 
